e6a99bcce93d72899b41afb9ed18852a4707ba43
sharon <sharonghae@gmail.com>
2016-11-12 19:03:04 -0500

    add demo and reset features

--- public/word.js
+++ public/word.js
@@ -1,10 +1,98 @@
 var categories = {};
+//var categoriesDemo = {};
+
+function reset() {
+    categories = {};
+    d3.select("#categories")
+        .selectAll("div")
+        .remove();
+    document.getElementById("mainInputText").value = null;
+    d3.select(".chart")
+        .selectAll("svg")
+        .remove();
+}
+
+function demoFill() {
+    document.getElementById("demo-click").remove();
+
+	var textArea = document.getElementById("mainInputText");
+    //var sampleTranscript;
+
+    $.get('/db01.txt', function(data, status){
+        textArea.value = data;
+
+        demoCategories = {
+            "Homeland Security": ["isis", "terrorists", "terrorism", "iraq", "islam"],
+            //"Education": ["schools", "school", "children", "college", "education", "tuition"],
+            //"Healthcare": ["medicare", "medicaid", "obamacare", "health insurance", "healthcare", "health"],
+            "Science": ["climate change", "global warming", "nasa", "technology"]
+        };
+
+        categories = demoCategories;
+
+        for(var key in demoCategories){
+            if(!demoCategories.hasOwnProperty(key)) continue;
+
+            var categoryId = key.replace(/\s+/g, '-');
+            var html = '<div id="category-' + categoryId + '"><br><strong>' + key + '</strong><a class="button small alert" onclick="removeCategory(this)">-</a>\
+				<div class="row"><div class="large-12 columns"><div class="row collapse"><div class="small-10 columns">\
+				<input type="text" id="search-term-' + categoryId + '" placeholder="Enter search term">\
+				</div><div class="small-2 columns">\
+				<a class="button" onclick="addSearchTerm(' + '\''+ key + '\',\'' + categoryId + '\'' + ')">+</a>\
+				</div></div></div></div></div>';
+
+            $("#categories").append(html);
+
+            for(var k = 0; k < demoCategories[key].length; k++) {
+                //categories[category].push(searchTerm);
+                d3.select("#category-" + categoryId)
+                    .append("div")
+                    .style("margin-left", "20px")
+                    .html('<em>' + demoCategories[key][k] + '</em>');
+            }
+        }
+        countSearchTerms();
+    });
+
+    // var client = new XMLHttpRequest();
+    // client.open('GET', '/db01.txt');
+    // client.onreadystatechange = function(){
+    //     sampleTranscript = client.responseText;
+    //
+    //     textArea.value = sampleTranscript;
+    //
+    //     var categoriesDemo = {
+    //         "Homeland Security": ["isis", "terrorists", "terrorism", "iraq", "islam"],
+    //         "Education": ["schools", "school", "children", "college", "education", "tuition"],
+    //         "Healthcare": ["medicare", "medicaid", "obamacare", "health insurance", "healthcare"],
+    //         "Science": ["climate change", "global warming", "nasa", "technology", "green energy"]
+    //     };
+    //
+    //     for(var key in categoriesDemo) {
+    //         debugger;
+    //         if(!categoriesDemo.hasOwnProperty(key)) continue;
+    //         //document.getElementById("category").value = key;
+    //         console.log(key);
+    //         //addCategory();
+    //         // var categoryID = key.replace(/\s+/g, '-');
+    //         // for (var k = 0; k < categoriesDemo[key].length; k++) {
+    //         //     document.getElementById("search-term-" + categoryID).value = categoriesDemo[key][k];
+    //         //     addSearchTerm(categoriesDemo[key], categoryID)
+    //         // }
+    //     }
+    // };
+}
+
+/*
+ * @author: 		SHARON CHOE
+ * @createdDate:	2016-11-12
+ * @purpose:		A word counter to see through categories and discussion points etc.....
+ */
 
 function addCategory() {
 	var category = document.getElementById("category").value.trim();
 
 	var categoryID = category.replace(/\s+/g, '-');
-	console.log(categoryID)
 	if(category !== "" && !categories[category]) {
 		categories[category] = [];
 		d3.select("#categories")
@@ -13,7 +101,7 @@ function addCategory() {
 			.attr("id", "category-" + categoryID)
 			.html('<br><strong>' + category + '</strong><a class="button small alert" onclick="removeCategory(this)">-</a>\
 				<div class="row"><div class="large-12 columns"><div class="row collapse"><div class="small-10 columns">\
-				<input type="text" id="search-term-' + categoryID + '">\
+				<input type="text" id="search-term-' + categoryID + '" placeholder="Enter search term">\
 				</div><div class="small-2 columns">\
 				<a class="button" onclick="addSearchTerm(' + '\''+ category + '\',\'' + categoryID + '\'' + ')">+</a>\
 				</div></div></div></div>');
@@ -48,13 +136,13 @@ function removeSearchTerm(element, category) {
 
 //where the bulk of the logic happens when user clicks 'Search' 
 function countSearchTerms() {
-	var searchTerms = [],
-		dictionary = {}, //searchTerm: counter
-		transcript = document.getElementById("inputString").value.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\r?\n|\r/g," ").toLowerCase().split(' '); //remove punctuations, line breaks/returns, lowercase
-	
+	var dictionary = {}, //searchTerm: counter
+		transcript = document.getElementById("mainInputText").value.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\r?\n|\r/g," ").toLowerCase().split(' '); //remove punctuations, line breaks/returns, lowercase
+
+	//TO DO: remove "/" when not testing
 	if(transcript == "") return;
 
-	var collapsedSearchTerms = collapseSearchTerms(searchTerms); //helper function
+	var collapsedSearchTerms = collapseSearchTerms(); //helper function
 
 	//Goal: decrease words to search thru by filtering out words not in range
 
@@ -80,35 +168,149 @@ function countSearchTerms() {
 	var categoriesCount = [];
 	for(var key in categories) {
 		var count = 0;
-		for (var i = 0; i < categories[key].length; i++) {
-			if(!dictionary[categories[key][i]]) continue;
-			else count += dictionary[categories[key][i]];
+		for (var j = 0; j < categories[key].length; j++) {
+			if(!dictionary[categories[key][j]]) continue;
+			else count += dictionary[categories[key][j]];
 
 		}
 		var obj = {"category": key, "count": count}
 		categoriesCount.push(obj)
 	}
+	//TO DO: createBarChart(categoriesCount) when not testing
 	createBarChart(categoriesCount);
 }
 
 //need to collapse searchTerms (& remove duplicates)
-function collapseSearchTerms(searchTerms) {
+function collapseSearchTerms() {
+    var ret = [];
 	for(var key in categories) {
-		searchTerms = _.union(searchTerms, categories[key]) //use lodash method to combine arrays and return array with unique values 
+        ret = _.union(ret, categories[key]) //use lodash method to combine arrays and return array with unique values
 	}
-	return searchTerms;
+	return ret;
 }
 
 
+//TO DO: createBarChart(categoriesCount) when not testing
 function createBarChart(categoriesCount) {
+	var fakeData = [{
+		"category": "Homeland Security",
+		"count": 10
+	},{
+		"category": "Science",
+		"count": 27
+	},{
+		"category": "Education",
+		"count": 15
+	},{
+		"category": "Women's Rights",
+		"count": 19
+	}];
+
 	d3.select(".chart")
-	  .selectAll("div")
+	  .selectAll("svg")
 	  .remove();
-	  
-	d3.select(".chart")
-	  .selectAll("div")
-	    .data(categoriesCount) //need to collect data!
-	  .enter().append("div")
-	    .style("width", function(d) { return d.count * 10 + "px"; })
-	    .text(function(d) { return `${d.category}: ${d.count}`; });
+
+	var div = d3.select("body").append("div").attr("class", "toolTip");
+	
+	var axisMargin = 20,
+		margin = 40,
+		valueMargin = 4,
+		width = parseInt(d3.select('.chart').style('width'), 10),
+		height = parseInt(d3.select('.chart').style('width'), 10) / 2,
+		barHeight = (height - axisMargin - margin * 2) * 0.4 / categoriesCount.length,
+		barPadding = (height - axisMargin - margin * 2) * 0.6 / categoriesCount.length,
+		bar, svg, max, scale, xAxis, labelWidth = 0;
+
+	max = d3.max(categoriesCount, function(d) {
+		return d.count;
+	});
+
+	svg = d3.select('.chart')
+		.append("svg")
+		.attr("width", width)
+		.attr("height", height);
+
+	bar = svg.selectAll("g")
+		.data(categoriesCount)
+		.enter()
+		.append("g");
+
+	bar.attr("class", "bar")
+		.attr("cx", 0)
+		.attr("transform", function(d, i){
+			return "translate(" + margin + "," + (i * (barHeight + barPadding) + barPadding) + ")";
+		});
+
+	bar.append("text")
+		.attr("class", "label")
+		.attr("y", barHeight / 2)
+		.attr("dy", ".35em") //vertical align middle
+		.text(function(d){
+			return d.category;
+		}).each(function() {
+		labelWidth = Math.ceil(Math.max(labelWidth, this.getBBox().width));
+	});
+
+	//scale = d3.scale.linear()
+	scale = d3.scaleLinear()
+		.domain([0, max])
+		.range([0, width - margin*2 - labelWidth]);
+
+	//xAxis = d3.svg.axis()
+	xAxis = d3.axisBottom()
+		.scale(scale)
+		.tickSize(-height + 2*margin + axisMargin);
+		//.orient("bottom");
+
+	bar.append("rect")
+		.attr("transform", "translate("+labelWidth+", 0)")
+		.attr("height", barHeight)
+		.attr("width", function(d){
+			return scale(d.count);
+		});
+
+	bar.append("text")
+		.attr("class", "value")
+		.attr("y", barHeight / 2)
+		.attr("dx", -valueMargin + labelWidth) //margin right
+		.attr("dy", ".35em") //vertical align middle
+		.attr("text-anchor", "end")
+		.text(function(d){
+			return (d.count);
+		})
+		.attr("x", function(d){
+			var width = this.getBBox().width;
+			return Math.max(width + valueMargin, scale(d.count));
+		});
+
+	bar
+		.on("mousemove", function(d){
+			div.style("left", d3.event.pageX+10+"px");
+			div.style("top", d3.event.pageY-25+"px");
+			div.style("display", "inline-block");
+			div.html((d.category)+"<br>"+(d.count));
+		});
+	bar
+		.on("mouseout", function(d){
+			div.style("display", "none");
+		});
+
+	svg.insert("g",":first-child")
+		.attr("class", "axisHorizontal")
+		.attr("transform", "translate(" + (margin + labelWidth) + ","+ (height - axisMargin - margin)+")")
+		.call(xAxis);
+
+	// d3.select(".chart")
+	//   	.selectAll("div")
+	// 	.data(fakeData) //need to collect data!
+	//   	.enter().append("div")
+	// 	.attr("x", function(d){
+	// 		return x
+	// 	});
+		// .attr("x", d => d.category)
+		// .attr("y", d => d.count)
+		// .attr("width", "10px")
+		// .attr("height", d => d.count * 10 + "px");
+	    //.style("width", function(d) { return d.count * 10 + "px"; })
+	    //.text(function(d) { return `${d.category}: ${d.count}`; });
 }
